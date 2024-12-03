88714d337aae42884f66d54bda280067dd8b658f
Satria-WS <extencezone02@gmail.com>
2024-12-03 16:31:20 +0700

    add visit us component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -17,10 +17,70 @@ import breakfast from "../../assets/home/ourMenu/breakfast.svg";
 import dishes from "../../assets/home/ourMenu/dishes.svg";
 import drink from "../../assets/home/ourMenu/drinks.svg";
 import desserts from "../../assets/home/ourMenu/desserts.svg";
+import { transform } from "typescript";
+import { Translate } from "@mui/icons-material";
+// image
+import image_2 from "../../assets/home/image-2.png";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import PlaceIcon from "@mui/icons-material/Place";
+
+// image event
+import catering from "../../assets/home/event/event-1.png";
+import birthday from "../../assets/home/event/event-2.png";
+import wedding from "../../assets/home/event/event-3.png";
+import event from "../../assets/home/event/event-4.png";
 
 const Home = () => {
+  const listIcon = [
+    {
+      imageList: breakfast,
+      title: "Breakfast",
+      text: "",
+      linkUrl: "",
+    },
+    {
+      imageList: dishes,
+      title: "Dishes",
+      text: "",
+      linkUrl: "",
+    },
+    {
+      imageList: drink,
+      title: "Drink",
+      text: "",
+      linkUrl: "",
+    },
+    {
+      imageList: desserts,
+      title: "Desserts",
+      text: "",
+      linkUrl: "",
+    },
+  ];
 
-  
+  const listEvent = [
+    {
+      imageList: catering,
+      title: "Catering",
+      text: "",
+    },
+    {
+      imageList: birthday,
+      title: "Birthday",
+      text: "",
+    },
+    {
+      imageList: wedding,
+      title: "Wedding",
+      text: "",
+    },
+    {
+      imageList: event,
+      title: "Event",
+      text: "",
+    },
+  ];
 
   return (
     <>
@@ -97,14 +157,16 @@ const Home = () => {
           </Box>
         </Box>
         {/* Our Menu */}
-        <Box sx={{
-          padding: "3rem 0"
-        }}>
+        <Box
+          sx={{
+            padding: "3rem 0",
+          }}
+        >
           <Typography
             variant="h2"
             sx={{
               fontFamily: "Playfair Display",
-              textAlign:"center"
+              textAlign: "center",
             }}
           >
             Browse Our Menu
@@ -118,14 +180,14 @@ const Home = () => {
             gap={10}
           >
             {/* Loop through the items to display multiple cards */}
-            {Array.from(Array(4)).map((_, index) => (
+            {/* {Array.from(Array(4)).map((_, index) => ( */}
+            {listIcon.map((icon, index) => (
               <Grid2
                 item
                 xs={2} // Full width on mobile
                 sm={6} // 50% width on small screens (tablets)
                 md={3} // 25% width on medium and larger screens (desktops)
                 key={index}
-              
               >
                 <Card
                   sx={{
@@ -136,7 +198,12 @@ const Home = () => {
                     maxWidth: 345,
                     boxShadow: 3,
                     borderRadius: 2,
-                    padding:'1.5rem'
+                    padding: "1.5rem",
+                    cursor: "pointer",
+                    transition: "transform 0.3s ease",
+                    "&:hover": {
+                      transform: "translate(0,-10px)",
+                    },
                   }}
                 >
                   {/* Card Media for Image */}
@@ -146,8 +213,8 @@ const Home = () => {
                       height: "100px",
                     }}
                     component="img"
-                    image={breakfast} // Your image source
-                    alt="Breakfast"
+                    image={icon.imageList} // Your image source
+                    alt={icon.title}
                   />
 
                   {/* Card Content */}
@@ -161,7 +228,7 @@ const Home = () => {
                   >
                     {/* Title */}
                     <Typography variant="h5" component="div">
-                      Breakfast
+                      {icon.title}
                     </Typography>
 
                     {/* Description */}
@@ -185,6 +252,115 @@ const Home = () => {
             ))}
           </Grid2>
         </Box>
+        {/* visit us */}
+        <Box
+          sx={{
+            height: "600px",
+          }}
+        >
+          <div className="grid grid-cols-2 bg-[#F9F9F7] h-full py-32 px-28">
+            {/* col-1 */}
+            <div className="relative">
+              <img
+                className=" border border-black object-cover w-full h-[36rem] rounded-lg  "
+                src={image_2}
+                alt="person"
+              />
+
+              <div className=" bg-[#474747] w-[300px] h-[200px] absolute bottom-0 right-0 text-sm text-white p-5 space-y-3 rounded-lg">
+                <Typography
+                  sx={{
+                    fontWeight: 600,
+                  }}
+                >
+                  Come and visit us
+                </Typography>
+                <div>
+                  <PhoneIcon />: 0813-8797-1837
+                </div>
+                <div>
+                  <EmailIcon />: extencezone02@gmail.com
+                </div>
+                <div className="text-justify">
+                  <PlaceIcon />: Sawangan Permai RT, RT.7RT.1/RW.9, Sawangan
+                  Baru, Kec. Sawangan, Kota Depok, Jawa Barat 16511
+                </div>
+              </div>
+            </div>
+            {/* col-2 */}
+            <div className=" space-y-5 p-16">
+              <Typography
+                variant="h3"
+                sx={{
+                  fontFamily: "Playfair Display",
+                }}
+              >
+                We provide healthy food for your family
+              </Typography>
+              <p className="text-[#2C2F24] font-semibold">
+                Our story began with a vision to create a unique dining
+                experience that merges fine dining, exceptional service, and a
+                vibrant ambiance. Rooted in city's rich culinary culture, we aim
+                to honor our local roots while infusing a global palate.
+              </p>
+              <p>
+                At place, we believe that dining is not just about food, but
+                also about the overall experience. Our staff, renowned for their
+                warmth and dedication, strives to make every visit an
+                unforgettable event.
+              </p>
+            </div>
+          </div>
+        </Box>
+        {/* offer event */}
+
+        <Grid2
+          container
+          spacing={2} // Adds spacing between grid items
+          justifyContent="center" // Centers the items horizontally
+          sx={{ marginTop: 4 }} // Adds margin at the top of the grid container
+          gap={10}
+        >
+          {/* Loop through the items to display multiple cards */}
+          {/* {Array.from(Array(4)).map((_, index) => ( */}
+          {listIcon.map((icon, index) => (
+            <Grid2
+              item
+              xs={2} // Full width on mobile
+              sm={6} // 50% width on small screens (tablets)
+              md={3} // 25% width on medium and larger screens (desktops)
+              key={index}
+            >
+              <Card>
+                <CardMedia
+                  sx={{
+                    width: "300px",
+                    height: "300px",
+                  }}
+                  component="img"
+                  image={""} // Your image source
+                  alt={""}
+                />
+                <CardContent
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  {/* Title */}
+                  <Typography variant="h5" component="div">
+                    Catering
+                  </Typography>
+
+                  {/* Description */}
+                  <p></p>
+                </CardContent>
+              </Card>
+            </Grid2>
+          ))}
+        </Grid2>
       </Container>
     </>
   );
